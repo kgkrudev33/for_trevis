1335c8fbf388a1a3d0119205e483d0426ba13a0a
kgkrudev33 <ru.dev33@trezub.ru>
2017-12-14 11:44:26 +0300

    destroy

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class PythonSomeTests(unittest.TestCase):
         self.assertTrue(True, True)
 
     def test_assert_upper(self):
-        self.assertEqual("TEST", "TEST")
+        self.assertEqual("TESTsdfsdf", "TEST")
 
     def test_assert_greater(self):
         self.assertGreater(10, 5)
