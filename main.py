5cff0802cbf2a43b378362751fef6e35d557fb86
kgkrudev33 <33017338+kgkrudev33@users.noreply.github.com>
2017-12-28 10:17:52 +0300

    bad commit

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class PythonSomeTests(unittest.TestCase):
         self.assertGreater(10, 9)
         
     def test_assert_greater(self):
-        self.assertGreater(20, 9)
+        self.assertGreater(1, 9)
     
     def tearDown(self):
         pass
